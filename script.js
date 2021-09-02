2a029b5a58febc33f36fc2ece824452a3d392f57
Victor Lacatus <georgevictorl@yahoo.com>
2021-09-02 21:38:41 +0300

    added UI

--- script.js
+++ script.js
@@ -1,72 +1,127 @@
-// Create function computerPlay
-
-function computerPlay() {
-  let choice = ['rock', 'paper', 'scissors']
-  return choice[Math.floor (Math.random() * choice.length)]
-}
-
-
-
-
-//Create function playRound(playerSelection, computerSelection) -- make playerSelection case insensitive
-//also return the results of playRound
-
-let computerSelection = computerPlay();
-
-let playerScore = 0 ;
+let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
 
-function playRound() {
+const container = document.getElementById('container');
+const messageCont = document.getElementById('msg-container');
+const message = document.getElementById('message');
+const buttons = document.getElementById('btn-container');
+const pScore = document.getElementById('infoPScore');
+const cScore = document.getElementById('infoCScore');
+
+
+
+
+function startGame(){
+  message.textContent= "Press any of the three buttons above to select your champion! Good Luck!";
+  playerScore = 0;
+  computerScore = 0;
+  buttons.setAttribute('style','display:flex');
+  messageCont.style.position = '';
+  messageCont.style.bottom = '';
+
+  window.addEventListener('click', function(e) {
+
+    // console.log('target: ' + e.target.value);
+    // console.log('value: ' + e.target.alt);
   
-  let playerSelection=prompt('Choose "rock", "paper" or "scissors" ');
-
-  if     (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors' || 
-          playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock'  ||
-          playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
-            playerScore += 1
-            alert('YOU WIN! COMPUTER CHOSE ' + computerSelection.toUpperCase() + '. ' + playerSelection.toUpperCase() + ' BEATS ' + computerSelection.toUpperCase() + '! Player score: ' + playerScore + ' , Computer score: ' + computerScore + ' .'  )
-     }
-
-  else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock' || 
-           playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper'  ||
-           playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
-            computerScore += 1
-            alert('YOU LOSE! COMPUTER CHOSE ' + computerSelection.toUpperCase() + '. ' + computerSelection.toUpperCase() + ' BEATS ' + playerSelection.toUpperCase() + '! Player score: ' + playerScore + ' , Computer score: ' + computerScore + ' .'  )
-  }
-
-  else if (playerSelection.toLowerCase() == computerSelection) {
-    alert('TIE GAME! TRY AGAIN! Player score: ' + playerScore + ' , Computer score: ' + computerScore + ' .'  )
-  }
-
-  else {
-    alert('PLEASE CHOOSE ROCK, PAPER OR SCISSORS')
-  }
-
+    function computerPlay() {
+      let choice = ['rock', 'paper', 'scissors']
+      return choice[Math.floor (Math.random() * choice.length)]
+    }
   
+      let computerSelection = computerPlay();
+  
+    function checkSelection() {
+      if (e.target.value || e.target.alt === 'rock') {
+        playerSelection = 'rock';
+      } else if (e.target.value || e.target.alt === 'paper') {
+        playerSelection = 'paper';
+      } else if (e.target.value || e.target.alt === 'scissors') {
+        playerSelection = 'scissors';
+      } else playerSelection = '';
+      return playerSelection;
+    }
+  
+    function endGame() {
+       
+      buttons.setAttribute('style', 'display: none');
+      messageCont.setAttribute('style', 'position:absolute');
+      messageCont.setAttribute('style', 'bottom:80');
+      const resetBtn = document.createElement('button');
+      resetBtn.textContent = 'RESET GAME';
+      resetBtn.setAttribute('id', 'resetBtn');
+      container.insertBefore(resetBtn,messageCont);
+      
+
+      function resetGame() {
+        playerScore = 0;
+        computerScore = 0;
+        pScore.textContent = 'Player: ' + playerScore;
+        cScore.textContent = 'Computer: ' + computerScore;
+        container.removeChild(resetBtn);
+        startGame();
+      }
+  
+      
+     
+      resetBtn.addEventListener('click', resetGame);
+      playerScore= 0 ;
+      computerScore= 0;
+      
+    }
+  
+    function playRound() {
+      if (playerSelection == 'rock' && computerSelection == 'scissors' || 
+              playerSelection == 'paper' && computerSelection == 'rock'  ||
+              playerSelection == 'scissors' && computerSelection == 'paper') {
+                playerScore += 1;
+                console.log('pscore:' +playerScore);
+                console.log('cscore:' +computerScore);
+                message.textContent = 'YOU WIN! COMPUTER CHOSE ' + computerSelection.toUpperCase() + '. ' + playerSelection.toUpperCase() + ' BEATS ' + computerSelection.toUpperCase() + '!' ;
+                pScore.textContent = 'Player: ' + playerScore;
+                cScore.textContent = 'Computer: ' + computerScore;
+      } else if (playerSelection == 'scissors' && computerSelection == 'rock' || 
+               playerSelection == 'rock' && computerSelection == 'paper'  ||
+               playerSelection == 'paper' && computerSelection == 'scissors') {
+                computerScore += 1;
+                console.log('pscore:' +playerScore);
+                console.log('cscore:' +computerScore);
+                message.textContent ='YOU LOSE! COMPUTER CHOSE ' + computerSelection.toUpperCase() + '. ' + computerSelection.toUpperCase() + ' BEATS ' + playerSelection.toUpperCase() + '!' ;
+                pScore.textContent = 'Player: ' + playerScore;
+                cScore.textContent = 'Computer: ' + computerScore;
+      } else if (playerSelection == computerSelection) {
+        message.textContent = 'TIE GAME! TRY AGAIN!' ;
+        console.log('pscore:' +playerScore);
+        console.log('cscore:' +computerScore);
+      } 
+  
+      if (playerScore == 5 || computerScore == 5) 
+        endGame();
+        
+    }
+  
+    
+  
+    checkSelection();
+  
+    playRound();
+
+    if (playerScore === 5 ) {
+      message.textContent = 'Congratulations! You beat the computer! You were the first to reach 5 points!';
+    } else if (computerScore === 5) {
+      message.textContent = 'Tough luck, you lost! Computer was the first to reach 5 points! Try Again!';
+    }
+  
+    
+  
+  })
 }
 
+startGame();
+
+    
 
 
 
-// Create function game() that loops playRound five times
 
-function game() {
-  playRound()
-  playRound()
-  playRound()
-  playRound()
-  playRound()
-  if (playerScore > computerScore) {
-    alert('Congratulations! You won the game! Final score - player: ' + playerScore + ', computer: ' + computerScore + '. Refresh the page to play again!')
-  }
-
-  else if (playerScore  < computerScore) {
-    alert('You lost! Try again! Final score - player:' + playerScore + ', computer: ' + computerScore + '. Refresh the page to play again!')
-  }
-
-  else {
-    alert('Tie Game! Final score - player:' + playerScore + ', computer: ' + computerScore + '. Refresh the page to play again!')
-  }
-}
-
-game()
