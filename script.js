6b22fa5508335ac508b7f08204792ebb306e21f3
Victor Lacatus <georgevictorl@yahoo.com>
2021-10-03 19:51:20 +0300

    Fix bubbling issue after clicking reset button by refreshing page after reset

--- script.js
+++ script.js
@@ -5,24 +5,41 @@ let playerSelection;
 let targetValue = '';
 let targetAlt = '';
 
+ function logText(e) {
+   console.log(this);
+ }
+
 const container = document.getElementById('container');
+buttonCont = document.getElementById('btn-container')
 const messageCont = document.getElementById('msg-container');
 const message = document.getElementById('message');
-const buttons = document.getElementById('btn-container');
+const buttons = document.getElementsByClassName('btn');
 const pScore = document.getElementById('infoPScore');
 const cScore = document.getElementById('infoCScore');
+const resetBtn = document.getElementById('resetBtn');
+const header = document.getElementById('header');
+
+//SET INITIAL VALUES
+playerSelection = '';
+playerScore = 0;
+computerScore = 0;
+pScore.textContent = 'Player: ' + playerScore;
+cScore.textContent = 'Computer: ' + computerScore;
+message.textContent= "Press any of the three buttons above to select your champion! Good Luck!";
+buttonCont.setAttribute('style','display:flex');
+messageCont.style.position = '';
+messageCont.style.bottom = '';
 
 // CREATE FUNCTION endGame
 function endGame() {
 
   // HIDE RPS BUTTONS AND CREATE RESET BUTTON
-  buttons.setAttribute('style', 'display: none');
+  buttonCont.setAttribute('style', 'display: none');
   messageCont.setAttribute('style', 'position:absolute');
   messageCont.setAttribute('style', 'bottom:80');
-  const resetBtn = document.createElement('button');
-  resetBtn.textContent = 'RESET GAME';
-  resetBtn.setAttribute('id', 'resetBtn');
-  container.insertBefore(resetBtn,messageCont);
+  resetBtn.setAttribute('style', 'display:block');
+  header.setAttribute('style', 'display:none');
+  message.setAttribute('style', 'color:white');
 
   // DECLARE OVERALL WINNER
   if (playerScore === 5 ) {
@@ -39,28 +56,12 @@ function endGame() {
 // CREATE FUNCTION FRESHGAME
 function freshGame() {
 
-  
-  //SET INITIAL VALUES
-  playerSelection = '';
-  playerScore = 0;
-  computerScore = 0;
-  pScore.textContent = 'Player: ' + playerScore;
-  cScore.textContent = 'Computer: ' + computerScore;
-  message.textContent= "Press any of the three buttons above to select your champion! Good Luck!";
-  buttons.setAttribute('style','display:flex');
-  messageCont.style.position = '';
-  messageCont.style.bottom = '';
-
-  window.addEventListener('click', function(e) {
+  let listenFunction = function(e) {
     
   // CREATE FUNCTION startGame
   function startGame() {
 
-  
-
-
     // STORE COMPUTER CHOICE IN VARIABLE CALLED computerSelection AND RETURN IT
-
     function computerPlay() {
       let choice = ['rock', 'paper', 'scissors']
       return choice[Math.floor (Math.random() * choice.length)]
@@ -68,35 +69,29 @@ function freshGame() {
 
     let computerSelection = computerPlay();
     
-
-  
     // STORE VALUE OF USER'S CLICK IN playerSelection AND RETURN IT
     function checkSelection() {
-      
-        if (e.target.value || e.target.alt === 'rock') {
+      if (e.target.value || e.target.alt === 'rock') {
           playerSelection = 'rock';
-        } else if (e.target.value || e.target.alt === 'paper') {
+      } else if (e.target.value || e.target.alt === 'paper') {
           playerSelection = 'paper';
-        } else if (e.target.value || e.target.alt === 'scissors') {
+      } else if (e.target.value || e.target.alt === 'scissors') {
           playerSelection = 'scissors';
-        } else playerSelection = '';
-        return playerSelection;
-      }
+      } else playerSelection = '';
+      return playerSelection;
+    }
 
     //CALL FUNCTION checkSelection;
     checkSelection(); 
     console.log(playerSelection);
 
-
-
-  // PLAYROUND FUNCTION
+    // PLAYROUND FUNCTION
     // COMPARE playerSelection TO computerSelection
     // MODIFY SCORE
     // DECLARE WINNER
     // DECLARE SCORE
-
-  function playRound() {
-    if (playerSelection == 'rock' && computerSelection == 'scissors' || 
+    function playRound() {
+      if (playerSelection == 'rock' && computerSelection == 'scissors' || 
                 playerSelection == 'paper' && computerSelection == 'rock'  ||
                 playerSelection == 'scissors' && computerSelection == 'paper') {
                 playerScore += 1;
@@ -105,42 +100,39 @@ function freshGame() {
                 message.textContent = 'YOU WIN! COMPUTER CHOSE ' + computerSelection.toUpperCase() + '. ' + playerSelection.toUpperCase() + ' BEATS ' + computerSelection.toUpperCase() + '!' ;
                 pScore.textContent = 'Player: ' + playerScore;
                 cScore.textContent = 'Computer: ' + computerScore;
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock' || 
-               playerSelection == 'rock' && computerSelection == 'paper'  ||
-               playerSelection == 'paper' && computerSelection == 'scissors') {
+      } else if (playerSelection == 'scissors' && computerSelection == 'rock' || 
+                      playerSelection == 'rock' && computerSelection == 'paper'  ||
+                      playerSelection == 'paper' && computerSelection == 'scissors') {
                 computerScore += 1;
                 console.log('pscore lose:' +playerScore);
                 console.log('cscore:' +computerScore);
                 message.textContent ='YOU LOSE! COMPUTER CHOSE ' + computerSelection.toUpperCase() + '. ' + computerSelection.toUpperCase() + ' BEATS ' + playerSelection.toUpperCase() + '!' ;
                 pScore.textContent = 'Player: ' + playerScore;
                 cScore.textContent = 'Computer: ' + computerScore;
-    } else if (playerSelection == computerSelection) {
-      console.log('pscore tie:' +playerScore);
-      console.log('cscore tie:' +computerScore);
-        message.textContent = 'TIE GAME! TRY AGAIN!' ;
-        pScore.textContent = 'Player: ' + playerScore;
-        cScore.textContent = 'Computer: ' + computerScore;
-    };
+      } else if (playerSelection == computerSelection) {
+                console.log('pscore tie:' +playerScore);
+                console.log('cscore tie:' +computerScore);
+                message.textContent = 'TIE GAME! TRY AGAIN!' ;
+                pScore.textContent = 'Player: ' + playerScore;
+                cScore.textContent = 'Computer: ' + computerScore;
+      };
 
     function checkEnd() {
       if (playerScore == 5 || computerScore == 5) {
 
-        function resetClick() {
-          container.removeChild(resetBtn);
-          freshGame();
+        endGame();
+
+        function refreshPage() {
+          window.location.reload();
         }
 
-        endGame();
-        resetBtn.addEventListener('click', resetClick);      
+        resetBtn.addEventListener('click', refreshPage);
+           
       }
     };
 
     //  CALL FUNCTION CHECKEND
-    checkEnd()
-    
-    
-    
-    
+    checkEnd();
   }
 
   //CALL FUNCTION playRound
@@ -148,10 +140,13 @@ function freshGame() {
     
   }
   startGame();
-  });
   
+  };
+  
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', listenFunction, false);
+  }
 }
-
 
 freshGame();
 
